7b89bf692976a23204f62d9f7f9b15ca380ac1ef
Peter Phan <peter_phan05@yahoo.com>
2021-06-03 02:18:48 -0700

    Sidebar Icons

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Compose from '../buttons/Compose';
+import { sidebarButtonItems } from '../data/SidebarButtonItems';
 
 const Sidebar = () => {
     return (
@@ -8,8 +9,13 @@ const Sidebar = () => {
             <ComposeWrapper>
                 <Compose />
             </ComposeWrapper>
-
-            <SideButtonWrappers></SideButtonWrappers>
+            <SideButtonWrappers>
+                {sidebarButtonItems.map(item => (
+                    <SidebarButtonItems>
+                        {item.icon} {item.text}
+                    </SidebarButtonItems>
+                ))}
+            </SideButtonWrappers>
 
             <MeetWrapper></MeetWrapper>
 
@@ -22,9 +28,35 @@ const Sidebar = () => {
 
 export default Sidebar;
 
-const Wrapper = styled.div``;
-const ComposeWrapper = styled.div``;
+const Wrapper = styled.div`
+    border-right: 1px solid lightgray;
+    height: 100vh;
+`;
+
+const ComposeWrapper = styled.div`
+    display: grid;
+    place-items: start;
+    padding: 10px 20px;
+`;
+
 const SideButtonWrappers = styled.div``;
+
+const SidebarButtonItems = styled.div`
+    display: grid;
+    grid-template-columns: 14% auto;
+    color: gray;
+    padding: 5px 25px;
+    border-radius: 0 100px 100px 0;
+    cursor: pointer;
+    margin-right: 8px;
+
+    :hover {
+        background-color: #f5f7f7;
+    }
+`;
+
 const MeetWrapper = styled.div``;
+
 const HangoutsWrapper = styled.div``;
+
 const BottomIconsWrapper = styled.div``;
