ec5a14b883cb0ec460faf53c2bab61eabfb70f81
Peter Phan <peter_phan05@yahoo.com>
2021-06-09 02:10:13 -0700

    Email Views

--- src/components/emailItem/EmailItem.js
+++ src/components/emailItem/EmailItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { Checkbox } from '@material-ui/core';
@@ -8,20 +8,48 @@ import StarIcon from '@material-ui/icons/Star';
 import IconButton from '@material-ui/core/IconButton';
 
 const EmailItem = ({ starred, from, subject, message, received, read }) => {
+    const [star, setStar] = useState(starred);
+
     return (
         <Wrapper>
             <Checkbox />
-            <IconButton onClick>
-                {starred ? (
-                    <StarIcon htmlColor="#f7cb69" />
-                ) : (
-                    <StarBorderIcon />
-                )}
+            <IconButton onClick={() => (star ? setStar(false) : setStar(true))}>
+                {star ? <StarIcon htmlColor="#f7cb69" /> : <StarBorderIcon />}
             </IconButton>
+
+            <p className={!read === false && 'unread'}>{from}</p>
+
+            <div>
+                <p className={!read && 'unread'}>{subject}</p> -{' '}
+                <span>{message}</span>
+            </div>
+
+            <p className={!read && 'unread'}>{received}</p>
         </Wrapper>
     );
 };
 
 export default EmailItem;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    padding-left: 20px;
+    border-top: 1px solid lightgray;
+    display: grid;
+    grid-template-columns: min-content min-content 120px auto min-content;
+    align-items: center;
+    cursor: pointer;
+    padding-right: 20px;
+
+    div {
+        display: flex;
+
+        span {
+            color: darkgray;
+        }
+    }
+
+    .unread {
+        color: black;
+        font-weight: bolder;
+    }
+`;
